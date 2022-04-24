9a5506d2eb5da527dbeaf8b530d85ce2cd2975a9
sungsu <jeehyuk97@daum.net>
2022-04-24 19:14:32 +0900

    unuseful heap implement

---
+++ Heap/SimpleHeap.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define TRUE		1
+#define FALSE		0
+
+#define HEAP_LEN	100
+
+typedef char HData;
+typedef int Priority;
+
+typedef struct _heapElem
+{
+	Priority pr;
+	HData data;
+} HeapElem;
+
+typedef struct _heap
+{
+	int numOfData;
+	HeapElem heapArr[HEAP_LEN];
+} Heap;
+
+void HeapInit(Heap* ph);
+int HIsEmpty(Heap* ph);
+
+void HInsert(Heap* ph, HData data);
+HData HDelete(Heap* ph);
